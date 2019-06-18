c7e155f02bd9ce80c6baed49593612b880f01c93
Simone <Simone@DESKTOP-EOLL1TL.homenet.telecomitalia.it>
2019-06-18 17:19:45 +0200

    Commit 35: Correzione Spring boot App, parte 2

--- Project_Farmacia/src/main/java/com/example/demo/ProjectFarmaciaApplication.java
+++ Project_Farmacia/src/main/java/com/example/demo/ProjectFarmaciaApplication.java
@@ -2,13 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @SpringBootApplication
 public class ProjectFarmaciaApplication {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectFarmaciaApplication.class, args);
-		Partita_Iva partita = new Partita_Iva();
 	}
 	
 
