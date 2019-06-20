c5e5a577912fe69e45a6ca3b8402e7e58e9c9343
Simone <Simone@DESKTOP-EOLL1TL.homenet.telecomitalia.it>
2019-06-20 17:27:06 +0200

    Commit 1.1.3:
    Modifica alla classe Output_Verifica_Stringhe

--- Project_Farmacia/src/main/java/com/example/demo/Output_Verifica_Stringhe.java
+++ Project_Farmacia/src/main/java/com/example/demo/Output_Verifica_Stringhe.java
@@ -2,32 +2,32 @@ package com.example.demo;
 
 public class Output_Verifica_Stringhe {
 	
-	private String stringa;
-	private int ripetizioni;
+	private String Cella;
+	private int Clone;
 	
-	public Output_Verifica_Stringhe(String stringa, int ripetizioni) {
-		this.stringa = stringa;
-		this.ripetizioni = ripetizioni;
+	public Output_Verifica_Stringhe(String Cella, int Clone) {
+		this.Cella = Cella;
+		this.Clone = Clone;
 	}
 
-	public String getStringa() {
-		return stringa;
+	public String getCella() {
+		return Cella;
 	}
 
-	public void setStringa(String stringa) {
-		this.stringa= stringa;
+	public void setCella(String Cella) {
+		this.Cella = Cella;
 	}
 
-	public int getRipetizioni() {
-		return ripetizioni;
+	public int getClone() {
+		return Clone;
 	}
 
-	public void setRipetizioni(int ripetizioni) {
-		this.ripetizioni = ripetizioni;
+	public void setClone(int Clone) {
+		this.Clone = Clone;
 	}
 
 	@Override
 	public String toString() {
-		return "Output_Verifica_Stringhe [Stringa=" + stringa + ", ripetizioni=" + ripetizioni + "]";
+		return "Output_Verifica_Stringhe [Cella=" + Cella + ", Clone=" + Clone + "]";
 	}
 }
