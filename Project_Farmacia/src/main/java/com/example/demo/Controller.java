5e0673422c0875891431ba64a59bc57220e086e7
Ziomike98 <48190785+Ziomike98@users.noreply.github.com>
2019-06-21 23:06:18 +0200

    Commit 1.2.0:
    -Classe Controller quasi completata;
    -Varie problematiche risolte.

--- Project_Farmacia/src/main/java/com/example/demo/Controller.java
+++ Project_Farmacia/src/main/java/com/example/demo/Controller.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.lang.reflect.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,19 +12,25 @@ import java.util.Date;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class Controller {
+public class Controller (File file) {
+
+	@RequestMapping(value="/metadati", method=RequestMethod.GET)
+	public ArrayList<Farmacie_Lazio_Metadati> RichestaMetadati(File file){
+		return Pars_Ser_Metadati.Lettura_Meta(new File("file metadati.dat"));
+	}
 	
 	/* 
 	 * Classe per la gestione delle funzioni di spring boot
 	 */
 	
 	@RequestMapping(value="/dati", method = RequestMethod.GET)
-	public Object Ricerca (@RequestParam(value="filtri", defaultValue = "empty")String filter, String attributo, String val1, 
-						   @RequestParam(value="valoreB", defaultValue = "0") String valoreB) {
-		
-		ArrayList <Farmacie_Lazio> List = new ArrayList<Farmacie_Lazio>();
+	public ArrayList<Farmacie_Lazio> Ricerca (@RequestParam(value="filtri", defaultValue = "empty")String filter, String attributo, String val1, @RequestParam(value="valoreB", defaultValue = "0") String valoreB) throws ParseException {
+        		
+		ArrayList<Farmacie_Lazio> lista= new ArrayList<Farmacie_Lazio>();
+		return Serializzazione_Dati.Serialization(new File("ListaFarmacieLazio.csv"), lista);
 	
-		
+		ArrayList<Farmacie_Lazio_Metadati> metadati = Pars_Ser_Metadati.Lettura_Meta(File file));
+		boolean[] checkAttributo = new boolean[metadati.size()];
 	}
 }
 
