713ce3a1806244238160bd4cca3807ba9c5aa64f
Ziomike98 <48190785+Ziomike98@users.noreply.github.com>
2019-06-22 15:16:31 +0200

    Commit 1.2.6: (Commit di Simone, problemi con Eclipse...)
    -Compilazione totale della Classe Controller.

--- Project_Farmacia/src/main/java/com/example/demo/Controller.java
+++ Project_Farmacia/src/main/java/com/example/demo/Controller.java
@@ -1,42 +1,116 @@
 package com.example.demo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.lang.reflect.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
+import com.example.demo.*;
 
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-public class Controller (File file) {
 
-	@RequestMapping(value="/metadati", method=RequestMethod.GET)
-	public ArrayList<Farmacie_Lazio_Metadati> RichestaMetadati(File file){
-		return Pars_Ser_Metadati.Serialization_Metadati(new File("file metadati.dat"), ArrayList<Farmacie_Lazio_Metadati> metadati);
+@RestController
+public class Controller {
+	
+	@RequestMapping(value="/metadata", method=RequestMethod.GET)
+	public ArrayList<Farmacie_Lazio_Metadati> RichiestaMeta(){
+		return Lettura_File.LetturaMetadati(new File("metadata file.dat"));
 	}
 	
-	/* 
-	 * Classe per la gestione delle funzioni di spring boot
-	 */
+	@RequestMapping(value="/data", method = RequestMethod.GET)
+	public Object DataRequest(@RequestParam(value = "filter", defaultValue = "vuoto")String filter, String attribute, String value1,@RequestParam(value="value2", defaultValue = "0") String value2){
+		ArrayList<Farmacie_Lazio> list = Lettura_File.LetturaDati(new File("data file.dat"));
+		ArrayList<Farmacie_Lazio_Metadati> metadata = Lettura_File.LetturaMetadati(new File("metadata file.dat"));
+		boolean[] TrovaAttributo = new boolean[metadata.size()];
+		for(int i=0; i<metadata.size(); i++) {
+			if(!attribute.equals(metadata.get(i).getName())) {
+				TrovaAttributo[i] = true;
+			}
+			else {
+				TrovaAttributo[i] = false;
+			}
+		}
+		boolean isNotAttribute = true;
+		for(int i=0; i<metadata.size(); i++) {
+			isNotAttribute &= TrovaAttributo[i];
+		}
+		if(isNotAttribute) {
+			return "Attributo inserito non valido";
+		}
+		if(filter.equals("vuoto"))
+			return list;
+		else {
+			Filtri filtro = new Filtri(filter);
+			if(!filtro.Esiste())
+				return "Filtro usato non esistente";
+			ArrayList<Farmacie_Lazio> output = (ArrayList<Farmacie_Lazio>)filtro.Ricerca(attribute, value1, value2, list);
+			if(output.size() == 0)
+				return "Nessun elemento corrisponde a questa richiesta";
+			else
+				return output;
+		}
+	}
 	
-	@RequestMapping(value="/dati", method = RequestMethod.GET)
-	public ArrayList<Farmacie_Lazio> Ricerca (@RequestParam(value="filtri", defaultValue = "empty")String filter, String attributo, String val1, @RequestParam(value="valoreB", defaultValue = "0") String valoreB) throws ParseException {
-        		
-		ArrayList<Farmacie_Lazio> lista= new ArrayList<Farmacie_Lazio>();
-		return Serializzazione_Dati.Serialization(new File("ListaFarmacieLazio.csv"), lista);
-	
-		ArrayList<Farmacie_Lazio_Metadati> metadati = Pars_Ser_Metadati.Lettura_Meta(File file));
-		boolean[] checkAttributo = new boolean[metadati.size()];
+	@RequestMapping(value="/statistics", method = RequestMethod.GET)
+	public Object StatisticsRequest(@RequestParam(value="filter", defaultValue = "vuoto")String filter, String attribute, String value1, @RequestParam(value = "value2", defaultValue = "0")String value2){
+		Vector<Object> attributes = new Vector<Object>();
+		try{
+			Method metodo = Farmacie_Lazio.class.getMethod("get" + attribute.substring(0,1).toUpperCase() + attribute.substring(1));
+			ArrayList<Farmacie_Lazio> list = Lettura_File.LetturaDati(new File("data file.dat"));
+			ArrayList<Farmacie_Lazio_Metadati> metadata = Lettura_File.LetturaMetadati(new File("metadata file.dat"));
+			boolean[] TrovaAttributo = new boolean[metadata.size()];
+			for(int i=0; i<metadata.size(); i++) {
+				if(!attribute.equals(metadata.get(i).getName())) {
+					TrovaAttributo[i] = true;
+				}
+				else {
+					TrovaAttributo[i] = false;
+				}
+			}
+			boolean isNotAttribute = true;
+			for(int i=0; i<metadata.size(); i++) {
+				isNotAttribute &= TrovaAttributo[i];
+			}
+			if(isNotAttribute) {
+				return "Attributo inserito non valido";
+			}
+			if(filter.equals("vuoto")) {
+				for(Farmacie_Lazio obj : list) {
+					attributes.add(metodo.invoke(obj));
+				}
+			}
+			else {
+				Filtri filtro = new Filtri(filter);
+				if(!filtro.Esiste())
+					return "Filtro usato non esistente";
+				ArrayList<Farmacie_Lazio> vFiltered = (ArrayList<Farmacie_Lazio>)filtro.Ricerca(attribute, value1, value2, list);
+				for(Farmacie_Lazio obj : vFiltered) {
+					attributes.add(metodo.invoke(obj));
+				}
+			}
+		} catch(NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch(IllegalAccessException e) {
+			e.printStackTrace();
+		} catch(InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		if(attributes.size() == 0)
+			return "Nessun elemento corrisponde a questa richiesta";
+		if(attributes.get(0) instanceof String) {
+			 ArrayList<String> list_string = new ArrayList<String>();
+			  for(Object obj : attributes) {
+				  String string = (String) obj;
+				  list_string.add(string);
+			  }
+			  return Verifica_Celle.getStatsString(list_string);
+		  }
+		else {
+			Vector<Double> vNumber = new Vector<Double>();
+			for(Object number : attributes)
+				vNumber.add((Double)number);
+			
+			return new NumStatistics(Statistics.getAvg(vNumber), Statistics.getMin(vNumber), Statistics.getMax(vNumber),
+					Statistics.getDevStd(vNumber), Statistics.getSum(vNumber), vNumber.size());
+		}
 	}
 }
-
-		
-		
-		
-
-    
-
