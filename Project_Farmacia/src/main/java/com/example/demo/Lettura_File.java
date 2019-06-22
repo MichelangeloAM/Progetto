b6809dbd1c1520df89cf5c5fa3eb5fa54f63b287
Simone <Simone@DESKTOP-EOLL1TL.homenet.telecomitalia.it>
2019-06-22 13:48:41 +0200

    Commit 1.2.2:
    -Modifica Classe Lettura_File.

--- Project_Farmacia/src/main/java/com/example/demo/Lettura_File.java
+++ Project_Farmacia/src/main/java/com/example/demo/Lettura_File.java
@@ -1,19 +1,40 @@
 package com.example.demo;
+
 import java.io.*;
+import java.util.*;
 import java.io.IOException;
 
 public class Lettura_File {
-   public static void Visualizza_Dati() {
-	   String ln = "";
-	   String Separatore = ";";
-	   try {
-		   
-		   BufferedReader br = new BufferedReader (new FileReader("C:\\Users\\UTENTE\\Desktop\\Programmazione-Progetto\\parafarmaciereglazio.csv"));
-		   ln = br.readLine(); //serve a non leggere la prima riga
-		   br.close();
-	   	   }
-	   catch ( IOException e) {
-		e.printStackTrace();
-	   }
-   }
+	
+	public static ArrayList<Farmacie_Lazio> LetturaDati(File file){
+		ArrayList<Farmacie_Lazio> l = new ArrayList<Farmacie_Lazio>();
+		try {
+			ObjectInputStream input = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)));
+			l = (ArrayList<Farmacie_Lazio>)input.readObject();
+			input.close();
+		} catch(ClassNotFoundException e) {
+			System.out.println("List Not Found");
+			e.printStackTrace();
+		} catch(IOException e) {
+			System.out.println("Error of I/O");
+			e.printStackTrace();
+		}
+		return l;
+	}
+	
+	public static ArrayList<Farmacie_Lazio_Metadati> LetturaMetadati(File file){
+		ArrayList<Farmacie_Lazio_Metadati> l = new ArrayList<Farmacie_Lazio_Metadati>();   
+		try {
+			ObjectInputStream input = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)));
+			l = (ArrayList<Farmacie_Lazio_Metadati>)input.readObject();
+			input.close();
+		} catch(ClassNotFoundException e) {
+			System.out.println("List Not Found");
+			e.printStackTrace();
+		} catch(IOException e) {
+			System.out.println("Error of I/O");
+			e.printStackTrace();
+		}
+		return l;
+	}
 }
